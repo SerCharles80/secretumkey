82fcc7a1dd9b7eb8ead17983b6ae302090507f43
carlo <carlo.carlini@blip.codes>
2025-02-13 16:09:56 +0100

    aggiornamento interfaccia grafica principale

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+
 const config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
+    width: 350,
+    height: 350,
+    parent: 'contenitore-gioco-phaser',
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
